b2b461b59ee2aea9d568659ff71c38f284340cd9
Vasilis Mirisas <vmirisas@gmail.com>
2022-09-26 21:00:08 +0300

    scanner for file input and read and a print that shows the result of the scanner

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-        InputSpecifier input = new InputSpecifier();
-        input.DataFileSpecification();
+public class Main {
+
+    private static Map<Integer, PlainData> dataLines = new HashMap<Integer, PlainData>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose the data file for encryption");
+        String fileLocation = scanner.nextLine();
+        try {
+            scanner = new Scanner(new FileReader(fileLocation));
+            scanner.useDelimiter("\t");
+            while(scanner.hasNextLine()) {
+                int id = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String name = scanner.next();
+                scanner.skip(scanner.delimiter());
+                String surname = scanner.next();
+                scanner.skip(scanner.delimiter());
+                String phonenumber = scanner.nextLine();
+                System.out.println("Imported id:" + id + ", name:" + name + ", surname:" + surname + ", phone number:" + phonenumber);
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try (FileWriter cypheredFile = new FileWriter("cyphered.txt")){
+
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+
+
     }
+
 
 
 }
---
+++ src/PlainData.java
@@ -0,0 +1,29 @@
+public class PlainData {
+    private int id;
+    private String name;
+    private String surname;
+    private String phonenumber;
+
+    public PlainData(int id, String name, String surname, String phonenumber) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phonenumber = phonenumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+}

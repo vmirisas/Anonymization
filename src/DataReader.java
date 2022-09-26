740fcc5247d3728060ccfb72dd76a13b9c64f71e
vmirisas <vmirisas@gmail.com>
2022-09-26 15:55:17 +0300

    Data reader input

--- src/DataReader.java
+++ src/DataReader.java
@@ -1,13 +1,30 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DataReader {
 
-private String dataFileLocation;
+    private Path dataFileLocation;
 
     public DataReader(String dataFileLocation) {
-        this.dataFileLocation = dataFileLocation;
+        this.dataFileLocation = Paths.get(dataFileLocation);
     }
 
-    public String getDataFileLocation() {
+    public Path getDataFileLocation() {
         return dataFileLocation;
+    }
+
+    public void fileCheck(File fileName) {
+        InputSpecifier input = "";
+        boolean exists = Files.exists(dataFileLocation);
+        if (exists){
+
+        } else {
+            System.out.println("This file does not exist, choose again");
+
+            input.DataFileSpecification();
+        }
     }
 
 

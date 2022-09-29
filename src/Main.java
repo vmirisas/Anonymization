d0048440b6d58e5a1b13be6e5a060d346d0c7808
vmirisas <vmirisas@gmail.com>
2022-09-29 12:04:05 +0300

    Refactored the way the data are being handled, with a 2d array instead of a Hashmap.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -35,44 +33,18 @@ public class Main {
             e.printStackTrace();
         }
 
-        int counter = dataFields.size();
+        int rows = dataValues.size() / dataFields.size();
+        int columns = dataFields.size();
+        int counter = 0;
 
-        //=========================  creating and populating the column arrays with their data  ========================
+        String[][] dataValuesMatrix = new String[columns][rows];
 
-        ArrayList<String> idArray = new ArrayList<>();
-        for (int i = dataFields.size()-counter; i < dataValues.size(); i+=dataFields.size()) {
-            idArray.add(dataValues.get(i));
+        for(int i = 0; i < columns; i++){
+            for (int j = 0; j < rows; j++){
+                dataValuesMatrix[i][j] = dataValues.get(counter);
+                counter++;
+            }
         }
-        counter = counter - 1;
-
-        ArrayList<String> nameArray = new ArrayList<>();
-        for (int i = dataFields.size()-counter; i < dataValues.size(); i+=dataFields.size()) {
-            nameArray.add(dataValues.get(i));
-        }
-        counter = counter - 1;
-
-        ArrayList<String> surnameArray = new ArrayList<>();
-        for (int i = dataFields.size()-counter; i < dataValues.size(); i+=dataFields.size()) {
-            surnameArray.add(dataValues.get(i));
-        }
-        counter = counter - 1;
-
-        ArrayList<String> phoneNumberArray = new ArrayList<>();
-        for (int i = dataFields.size()-counter; i < dataValues.size(); i+=dataFields.size()) {
-            phoneNumberArray.add(dataValues.get(i));
-        }
-
-        HashMap<String, String[]> idColumn = new HashMap<>();
-        idColumn.put(dataFields.get(0), idArray.toArray(new String[0]));
-
-        HashMap<String, String[]> nameColumn = new HashMap<>();
-        nameColumn.put(dataFields.get(1), nameArray.toArray(new String[0]));
-
-        HashMap<String, String[]> surnameColumn = new HashMap<>();
-        surnameColumn.put(dataFields.get(2), surnameArray.toArray(new String[0]));
-
-        HashMap<String, String[]> phoneNumberColumn = new HashMap<>();
-        phoneNumberColumn.put(dataFields.get(3), phoneNumberArray.toArray(new String[0]));
 
 
         System.out.println("Choose the configuration file");
@@ -97,40 +69,41 @@ public class Main {
             e.printStackTrace();
         }
 
+        ArrayList<Integer> sameFieldIndexArray = new ArrayList<>();
+        sameFieldIndexArray = SameFieldIndex(dataFields,configuredFields);
+        System.out.println(sameFieldIndexArray);
+
         //Write the encrypted values to a .txt file
         try (BufferedWriter cypheredFile = new BufferedWriter(new FileWriter("cyphered.txt")))
         {
             cypheredFile.write("ID" + "\t" + "NAME" + "\t" + "SURNAME" + "\t" + "PHONE_NUMBER"+ "\n");
             for (int i = 0; i < dataValues.size()/ dataFields.size(); i++){
-                cypheredFile.write(Arrays.toString(idColumn.get(i)) + "\t" + Arrays.toString(nameColumn.get(i)) + "\t" + Arrays.toString(surnameColumn.get(i)) + "\t" + Arrays.toString(phoneNumberColumn.get(i)) + "\n");
+                //cypheredFile.write(idColumn.get("id") + "\t" + Arrays.toString(nameColumn.get(i)) + "\t" + Arrays.toString(surnameColumn.get(i)) + "\t" + Arrays.toString(phoneNumberColumn.get(i)) + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        System.out.println(Arrays.toString(idColumn.get("id")));
-        System.out.println(Arrays.toString(nameColumn.get("name")));
-        System.out.println(Arrays.toString(surnameColumn.get("surname")));
-        System.out.println(Arrays.toString(phoneNumberColumn.get("phonenumber")));
-//        System.out.println(dataFields);
-//        System.out.println(dataFields.size());
-//
-//        System.out.println(dataValues);
-//        System.out.println(dataValues.size());
-//
-//        System.out.println(idArray);
-//        System.out.println(idArray.size());
-//
-//        System.out.println(nameArray);
-//        System.out.println(surnameArray);
-//        System.out.println(phoneNumberArray);
-
-
+        for(int i = 0; i < columns; i++){
+            for (int j = 0; j <rows; j++){
+                System.out.println("arr[" + i + "][" + j + "] = "+ dataValuesMatrix[i][j]);
+            }
+        }
 
 
     }
 
-
+    public static ArrayList<Integer> SameFieldIndex(ArrayList dataFields, ArrayList configuredFields){
+        ArrayList<Integer> sameFieldIndex = new ArrayList<>();
+        for(int i = 0; i < dataFields.size(); i++){
+            for (int j = 0; j < configuredFields.size(); j++){
+                if(dataFields.get(i).equals(configuredFields.get(j))){
+                    sameFieldIndex.add(dataFields.indexOf(dataFields.get(i)));
+                }
+            }
+        }
+        return sameFieldIndex;
+    }
 
 }
